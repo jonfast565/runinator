0ab9b7f91ede6a690c4c96daee009dd6af996e59
Jonathan Fast <128733927+jfastwhiskerlabs@users.noreply.github.com>
2025-01-16 00:59:35 -0500

    making the console plugin work... pita

--- runinator-plugin-console/src/lib.rs
+++ runinator-plugin-console/src/lib.rs
@@ -1,5 +1,7 @@
-use std::ffi::{c_char, c_int};
-use log::info;
+mod model;
+
+use std::{ffi::{c_char, c_int}, io::{BufRead, BufReader, Stderr}, process::{Command, Stdio}, thread, time};
+use log::{error, info};
 use runinator_utilities::{ffiutils, logger};
 
 const NAME: &str = "Console\0"; // Null-terminated string
@@ -23,5 +25,37 @@ extern "C" fn call_service(action_function: *const c_char, args: *const c_char) 
 
     info!("Running action '{}' w/ args `{}`", call_str, args_str);
 
+    const TIMEOUT_SECONDS : i32 = 5;
+    
+    let mut command = Command::new("cmd");
+    command.args(["/C", &args_str]);
+    command.stdout(Stdio::piped());
+    command.stderr(Stdio::piped());
+
+    let mut child = command.spawn().unwrap();
+    let stdout = child.stdout.take().unwrap();
+    let stderr = child.stderr.take().unwrap();
+
+    let timeout_thread = thread::spawn(move || {
+        for _ in 0..TIMEOUT_SECONDS {
+            if let Ok(Some(_)) = child.try_wait() {
+                return;
+            }
+            thread::sleep(time::Duration::from_secs(1));
+        }
+        child.kill().unwrap();
+    });
+
+    let stdout_reader = BufReader::new(stdout);
+    for line in stdout_reader.lines() {
+        info!("{}", line.unwrap());
+    }
+
+    let stderr_reader = BufReader::new(stderr);
+    for line in stderr_reader.lines() {
+        error!("{}", line.unwrap());
+    }
+
+    timeout_thread.join().unwrap();
     0
 }
