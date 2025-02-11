000c0774e789e56b474b82b15dedceadb8ffceb2
Jonathan Fast <128733927+jfastwhiskerlabs@users.noreply.github.com>
2025-02-11 15:02:34 -0500

    name change for model

--- runinator-plugin-console/src/model.rs
+++ runinator-plugin-console/src/model.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 
 #[derive(Serialize, Deserialize)]
-pub struct CommandLine {
+pub struct CommandLineParameters {
     pub command: String,
     pub args: String
 }
