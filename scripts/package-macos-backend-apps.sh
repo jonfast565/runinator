eb1f51430923953cfd804766322e8c8fba12c1e4
Jonathan Fast <jnfstdj656@gmail.com>
2026-09-14 00:01:07 -0400

    Align Windows graphics dependencies

--- scripts/package-macos-backend-apps.sh
+++ scripts/package-macos-backend-apps.sh
@@ -158,7 +158,7 @@ for app in "${apps[@]}"; do
   cat > "$config_path" <<EOF
 name = "$binary"
 product-name = "$product_name"
-version = "${RUNINATOR_VERSION:-0.34.747}"
+version = "${RUNINATOR_VERSION:-0.34.748}"
 identifier = "$identifier"
 description = "$description"
 formats = ["app"]
