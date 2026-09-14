1e8edc0d4b1dbf94004a5f3b368a87f293231219
Jonathan Fast <jnfstdj656@gmail.com>
2026-09-14 02:36:07 -0400

    Bump version to 0.34.750

--- scripts/package-macos-backend-apps.sh
+++ scripts/package-macos-backend-apps.sh
@@ -158,7 +158,7 @@ for app in "${apps[@]}"; do
   cat > "$config_path" <<EOF
 name = "$binary"
 product-name = "$product_name"
-version = "${RUNINATOR_VERSION:-0.34.749}"
+version = "${RUNINATOR_VERSION:-0.34.750}"
 identifier = "$identifier"
 description = "$description"
 formats = ["app"]
